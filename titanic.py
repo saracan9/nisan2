696b2d87509ccc1395af45f21cffa7fadc53ec19
Şara Can <saracan67@gmail.com>
2025-04-18 18:48:37 +0300

    KNN algoritması eklendi

--- titanic.py
+++ titanic.py
@@ -68,3 +68,25 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 print("\n Yeni Accuracy:", accuracy_score(y_test, y_pred_optimized))
 print("\n Yeni Confusion Matrix:\n", confusion_matrix(y_test, y_pred_optimized))
 print("\n Yeni Classification Report:\n", classification_report(y_test, y_pred_optimized))
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn_param_grid = {
+    'n_neighbors': list(range(1, 31)),
+    'weights': ['uniform', 'distance'],
+    'metric': ['euclidean', 'manhattan']
+}
+
+knn = KNeighborsClassifier()
+knn_grid = GridSearchCV(knn, knn_param_grid, cv=5, scoring='f1')
+knn_grid.fit(X_train, y_train)
+
+best_knn = knn_grid.best_estimator_
+print("\n KNN için en iyi parametreler:", knn_grid.best_params_)
+
+y_pred_knn = best_knn.predict(X_test)
+
+print("\n KNN Doğruluk (Accuracy):", accuracy_score(y_test, y_pred_knn))
+print("\n KNN Confusion Matrix:\n", confusion_matrix(y_test, y_pred_knn))
+print("\n KNN Classification Report:\n", classification_report(y_test, y_pred_knn))
